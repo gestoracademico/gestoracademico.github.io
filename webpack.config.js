b69cb0e27835210a2fb633a4666b27fa054ba8e0
tagleefren0618 <efren_tp05@hotmail.com>
2024-05-18 23:22:10 -0600

    testing

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource' // Utiliza 'asset/resource' para copiar los archivos de imagen sin procesar
+      }
+    ]
+  },
+  mode: 'development'
+};
